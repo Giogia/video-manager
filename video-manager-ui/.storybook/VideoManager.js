d5433e7eede64b3b0243e7c6e75e09a493cf3656
Giovanni Tommasi <giovannitommasi95@gmail.com>
2022-11-30 01:12:20 +0100

    added storybook and mui design

---
+++ video-manager-ui/.storybook/VideoManager.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming'
+
+export default create({
+    base: 'light',
+    brandTitle: 'Video Manager',
+    brandTarget: '_self',
+
+    appBorderRadius: 8
+})
---
+++ video-manager-ui/.storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons'
+import VideoManager from './VideoManager'
+
+addons.setConfig({
+    theme: VideoManager
+})
