eb1c983d06ab5c12cb687f721f2c2d11be8fce11
Giovanni Tommasi <giovannitommasi95@gmail.com>
2022-12-05 23:37:18 +0100

    added relay, app theming

--- video-manager-ui/src/App.tsx
+++ video-manager-ui/src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from 'react'
+import { ThemeProvider } from '@mui/material'
+import Container from '@mui/material/Container'
+
+import { Header } from './components/Header'
+import { Explorer } from './components/Explorer'
+import { lightTheme } from './themes/light.theme'
+import { darkTheme } from './themes/dark.theme'
 
 function App() {
+  const [theme, setTheme] = useState(lightTheme)
+
+  const { palette: { mode, background } } = theme
+
+  const handleTheme = () => setTheme(mode === 'dark' ? lightTheme : darkTheme)
+
   return (
     <div className="App">
       <header className="App-header">
+        <ThemeProvider theme={theme}>
+          <Container sx={{
+            height: '100vh',
+            bgcolor: background.default
+          }}>
+            <Header theme={mode} handleTheme={handleTheme} />
+            <Explorer content={[]} path='/' />
+          </Container>
+        </ThemeProvider>
       </header>
     </div>
   )
