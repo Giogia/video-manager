253dc34694d0cbabe8dab23c053f20dc4b704e11
Giovanni Tommasi <giovannitommasi95@gmail.com>
2022-12-05 01:17:42 +0100

    updated mui themes

---
+++ video-manager-ui/src/themes/dark.theme.tsx
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material'
+
+export const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        background: {
+            default: '#202124',
+            paper: '#333'
+        }
+    }
+})
---
+++ video-manager-ui/src/themes/light.theme.tsx
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material'
+
+export const lightTheme = createTheme({
+    palette: {
+        mode: 'light',
+        background: {
+            paper: 'whitesmoke',
+            default: 'white'
+        }
+    }
+})
