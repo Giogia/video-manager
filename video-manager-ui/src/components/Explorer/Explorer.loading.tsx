cb1c0ef92e981452c44b6b18dc2d9945daebdf85
Giovanni Tommasi <giovannitommasi95@gmail.com>
2022-12-09 20:17:33 +0100

    added circular progress to loading

--- video-manager-ui/src/components/Explorer/Explorer.loading.tsx
+++ video-manager-ui/src/components/Explorer/Explorer.loading.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import CircularProgress from '@mui/material/CircularProgress'
+
 import { Explorer } from "."
 
 /**
  * Component wrapper for loading stage
  */
 export const ExplorerLoading = () => (
-    <Explorer loading content='Loading...' />
+    <Explorer loading content={
+        <>
+            <CircularProgress/>
+            {'Loading...'}
+        </>
+    } />
 )
