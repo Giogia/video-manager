258d66ef7c6f31c145615db8505c087f888eb68a
Giovanni Tommasi <giovannitommasi95@gmail.com>
2022-12-17 19:05:15 +0100

    added item name to folder drag

--- video-manager-ui/src/components/Folder/Folder.drag.tsx
+++ video-manager-ui/src/components/Folder/Folder.drag.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDrag } from 'react-dnd'
+import Box from '@mui/material/Box'
 
 import { Folder, FolderProps } from './Folder.ui'
 
@@ -8,20 +9,23 @@ import { Folder, FolderProps } from './Folder.ui'
  */
 export const FolderWithDrag = ({ ...props }: FolderProps) => {
 
+  const { name } = props
+
   const [{ isDragging }, drag] = useDrag(() => ({
     type: 'Folder',
+    item: { name },
     collect: monitor => ({ isDragging: !!monitor.isDragging() })
   }))
 
   return (
-    <div ref={drag}
+    <Box ref={drag}
       style={{
         opacity: isDragging ? 0 : 1,
         transform: 'translate(0, 0)',
         transition: 'opacity 0.2s ease-in-out'
       }}>
       <Folder {...props} />
-    </div>
+    </Box>
   )
 }
 
