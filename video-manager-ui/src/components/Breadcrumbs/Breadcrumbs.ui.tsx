2eae75b39bf46ae01441dfc70b531da2c2917c0f
Giovanni Tommasi <giovannitommasi95@gmail.com>
2022-12-22 00:46:38 +0100

    fix breadcrumbs home button

--- video-manager-ui/src/components/Breadcrumbs/Breadcrumbs.ui.tsx
+++ video-manager-ui/src/components/Breadcrumbs/Breadcrumbs.ui.tsx
@@ -20,7 +20,7 @@ export interface BreadcrumbsProps {
 export const Breadcrumbs = ({ path }: BreadcrumbsProps) => {
 
   const levels = path.split(SEPARATOR)
-  const href = (level: number) => levels.slice(0, level + 1).join(SEPARATOR)
+  const href = (level: number) => levels.slice(0, level + 1).join(SEPARATOR) || SEPARATOR
 
   return (
     <BreadcrumbsMui
@@ -29,21 +29,23 @@ export const Breadcrumbs = ({ path }: BreadcrumbsProps) => {
       itemsAfterCollapse={2}
     >
       {levels
-        .map((link, i) => !link && i === 0 ?
-          <Button
-            icon='home'
-            size='small'
-          /> :
-          <Box sx={{ padding: '8px' }}>
-            <Link
-              underline='hover'
-              href={href(i)}
-              key={i}
-            >
-              {link}
-            </Link>
-          </Box>
-        )}
+        .map((link, i) => (
+          <Link
+            underline='hover'
+            href={href(i)}
+            key={i}
+          >
+            {!link && i === 0 ?
+              <Button
+                icon='home'
+                size='small'
+              /> :
+              <Box sx={{ padding: '8px' }}>
+                {link}
+              </Box>
+            }
+          </Link>
+        ))}
     </BreadcrumbsMui>
   )
 }
