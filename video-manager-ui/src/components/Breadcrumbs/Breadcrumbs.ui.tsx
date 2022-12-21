406e09bf6fce98d846ec225ebdce37f7b0cca629
Giovanni Tommasi <giovannitommasi95@gmail.com>
2022-12-22 00:20:31 +0100

    added home icon to breadcrumbs

--- video-manager-ui/src/components/Breadcrumbs/Breadcrumbs.ui.tsx
+++ video-manager-ui/src/components/Breadcrumbs/Breadcrumbs.ui.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import Box from '@mui/material/Box'
 import BreadcrumbsMui from '@mui/material/Breadcrumbs'
 import Link from '@mui/material/Link'
+
+import { Button } from '../Button'
 
 const SEPARATOR = '/'
 
@@ -26,15 +29,21 @@ export const Breadcrumbs = ({ path }: BreadcrumbsProps) => {
       itemsAfterCollapse={2}
     >
       {levels
-        .map((link, i) => (
-          <Link
-            underline='hover'
-            href={href(i)}
-            key={i}
-          >
-            {link}
-          </Link>
-        ))}
+        .map((link, i) => !link && i === 0 ?
+          <Button
+            icon='home'
+            size='small'
+          /> :
+          <Box sx={{ padding: '8px' }}>
+            <Link
+              underline='hover'
+              href={href(i)}
+              key={i}
+            >
+              {link}
+            </Link>
+          </Box>
+        )}
     </BreadcrumbsMui>
   )
 }
