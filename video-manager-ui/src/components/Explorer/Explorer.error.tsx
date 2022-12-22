e8c33a316323bdddb57953af78ef2194c7addd8f
Giovanni Tommasi <giovannitommasi95@gmail.com>
2022-12-22 19:15:38 +0100

    added refresh to explorer error

--- video-manager-ui/src/components/Explorer/Explorer.error.tsx
+++ video-manager-ui/src/components/Explorer/Explorer.error.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import Chip from "@mui/material/Chip"
+import { useNavigate } from 'react-router'
 
 import { Explorer } from "./Explorer.ui"
 
 /**
  * Component wrapper for error stage
  */
-export const ExplorerError = ({ error, resetErrorBoundary }: Record<string, any>) => (
-    <Explorer error
-        content={
-            <>
-                <pre>{error.message}</pre>
-                <Chip
-                    label="Try Again"
-                    onClick={resetErrorBoundary}
-                />
-            </>
-        }
-    />
-)
+export const ExplorerError = ({ error, resetErrorBoundary }: Record<string, any>) => {
+
+    const navigate = useNavigate()
+
+    return (
+        <Explorer error
+            content={
+                <>
+                    <pre>{error.message}</pre>
+                    <Chip
+                        label="Try Again"
+                        onClick={() => {
+                            resetErrorBoundary()
+                            navigate(0)
+                        }}
+                    />
+                </>
+            }
+        />
+    )
+}
