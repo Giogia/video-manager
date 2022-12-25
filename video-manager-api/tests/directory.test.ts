8067e6290f7316ab2d1331df8a698d7c04683b27
Giovanni Tommasi <giovannitommasi95@gmail.com>
2022-12-25 22:18:38 +0100

    added tests

---
+++ video-manager-api/tests/directory.test.ts
@@ -0,0 +1,46 @@
+import "reflect-metadata"
+import { graphql, GraphQLSchema } from "graphql"
+import { buildSchema } from 'type-graphql'
+
+import { DirectoryModel } from '../schema/directory'
+import { DirectoryResolver } from '../resolvers/directory'
+
+const mockingoose = require("mockingoose")
+
+describe('Resolvers', () => {
+
+    const model = mockingoose(DirectoryModel)
+
+    let schema: GraphQLSchema
+
+    beforeAll(async () => {
+        schema = await buildSchema({ resolvers: [DirectoryResolver] })
+    })
+
+    const doc = {
+        name: 'gio',
+        path: '/',
+        children: []
+    }
+
+    model.toReturn(doc, 'findOne')
+
+    test('getDirectory', async () => {
+        const query = `
+            query {
+                getDirectory(input: { path: "/", name: "gio" }){
+                    name
+                    path
+                    children {
+                        name
+                        path
+                    }
+                }
+            }
+        `
+
+        const { data } = await graphql(schema, query)
+
+        expect(data).toEqual({ getDirectory: doc })
+    })
+})
