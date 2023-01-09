884cb862ff65d7623b731cb32bf5c3a6211bad06
Giovanni Tommasi <giovannitommasi95@gmail.com>
2023-01-09 22:40:23 +0100

    added upload to button component

---
+++ video-manager-ui/src/components/Button/Button.upload.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react'
+
+import { Button, ButtonProps } from './Button.ui'
+
+/**
+ * Upload file logic
+ */
+const handleChange = (action: ButtonProps['action']) =>
+  (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (
+      event.target.files !== null &&
+      event.target?.files?.length > 0
+    ) {
+      action && action(event)
+    }
+  }
+
+/**
+ * Component Wrapper for uploading files
+ */
+export const ButtonWithUpload = ({ action, ...props }: ButtonProps) => {
+
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  return (
+    <>
+      <input
+        accept={'video/*'}
+        onChange={handleChange(action)}
+        ref={inputRef}
+        type="file"
+        style={{ display: 'none' }}
+      />
+      <Button {...props}
+        action={() => inputRef.current?.click()}
+      />
+    </>
+  )
+}
+
+ButtonWithUpload.defaultProps = {
+  ...Button.defaultProps
+}
