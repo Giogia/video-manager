b185b5bc52fcdcc1dea4e1fc2bba6ab30b97d6e9
Giovanni Tommasi <giovannitommasi95@gmail.com>
2023-03-27 01:38:19 +0200

    added playwright configuration

--- video-manager-ui/src/App/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import { AppWithRouter } from './App.router'
-
-test('renders learn react link', () => {
-  render(<AppWithRouter />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
