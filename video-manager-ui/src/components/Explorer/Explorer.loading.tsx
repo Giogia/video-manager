684c1582dadb6cf9075179950a3906fa0436ae2d
Giovanni Tommasi <giovannitommasi95@gmail.com>
2023-04-25 15:30:40 +0200

    added dynamic explorer loading skeleton

--- video-manager-ui/src/components/Explorer/Explorer.loading.tsx
+++ video-manager-ui/src/components/Explorer/Explorer.loading.tsx
@@ -5,15 +5,16 @@ import { WithFetchProps } from './Explorer.fetch'
 import { Folder } from '../Folder'
 import { ExplorerWithError } from './Explorer.error'
 
+const length = Math.floor((window.innerWidth - 100) / 100)
+
 /**
  * Component wrappers for loading stage
  */
 export const ExplorerLoading = () => (
     <Explorer loading
-        content={
-            Array.from({ length: 10 }).map((_, i) =>
-                <Folder loading />
-            )
+        content={Array
+            .from({ length })
+            .map(() => <Folder loading />)
         }
     />
 )
