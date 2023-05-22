a8344006df6749168cc35032d594bfc5bb3a19f0
Giovanni Tommasi <giovannitommasi95@gmail.com>
2023-05-23 00:27:03 +0200

    removed unused storybook dependencies

--- video-manager-ui/.storybook/main.js
+++ video-manager-ui/.storybook/main.js
@@ -10,7 +10,6 @@ export default {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
-    "storybook-addon-material-ui",
   ],
   features: {
     interactionsDebugger: true
