1ba46968826512a59f47b7600d7091c5a9e30568
Giovanni Tommasi <giovannitommasi95@gmail.com>
2023-06-02 18:59:05 +0200

    fixed storybook in compose.yaml and dockerfile, playright default browsers

--- video-manager-ui/playwright.config.ts
+++ video-manager-ui/playwright.config.ts
@@ -20,22 +20,23 @@ export default defineConfig({
 
    /* Configure projects for major browsers */
    projects: [
-      // {
-      //    name: 'chromium',
-      //    use: { ...devices['Desktop Chrome'] },
-      // },
+      {
+         name: 'chromium',
+         use: { ...devices['Desktop Chrome'] },
+      },
 
-      // {
-      //    name: 'firefox',
-      //    use: { ...devices['Desktop Firefox'] },
-      // },
+      {
+         name: 'firefox',
+         use: { ...devices['Desktop Firefox'] },
+      },
 
-      // {
-      //    name: 'webkit',
-      //    use: { ...devices['Desktop Safari'] },
-      // },
+      {
+         name: 'webkit',
+         use: { ...devices['Desktop Safari'] },
+      },
 
       /* Test against mobile viewports. */
+
       // {
       //   name: 'Mobile Chrome',
       //   use: { ...devices['Pixel 5'] },
@@ -46,13 +47,14 @@ export default defineConfig({
       // },
 
       /* Test against branded browsers. */
+
       // {
       //   name: 'Microsoft Edge',
       //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
       // },
-      {
-         name: 'Google Chrome',
-         use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-      }
+      // {
+      //    name: 'Google Chrome',
+      //    use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+      // }
    ]
 })
