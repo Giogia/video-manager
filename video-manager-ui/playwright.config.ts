7fe5cf0176decd9a1bfb3964bd2ebb21e4f0514c
Giovanni Tommasi <giovannitommasi95@gmail.com>
2023-06-10 19:34:01 +0200

    edited playwright config

--- video-manager-ui/playwright.config.ts
+++ video-manager-ui/playwright.config.ts
@@ -20,41 +20,17 @@ export default defineConfig({
 
    /* Configure projects for major browsers */
    projects: [
-      {
-         name: 'chromium',
-         use: { ...devices['Desktop Chrome'] },
-      },
 
+      /* Test against branded browsers. */
       {
-         name: 'firefox',
-         use: { ...devices['Desktop Firefox'] },
-      },
-
-      {
-         name: 'webkit',
-         use: { ...devices['Desktop Safari'] },
+         name: 'Chrome',
+         use: { ...devices['Desktop Chrome'], channel: 'chrome' },
       },
 
       /* Test against mobile viewports. */
-
-      // {
-      //   name: 'Mobile Chrome',
-      //   use: { ...devices['Pixel 5'] },
-      // },
-      // {
-      //   name: 'Mobile Safari',
-      //   use: { ...devices['iPhone 12'] },
-      // },
-
-      /* Test against branded browsers. */
-
-      // {
-      //   name: 'Microsoft Edge',
-      //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-      // },
-      // {
-      //    name: 'Google Chrome',
-      //    use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-      // }
+      {
+         name: 'Mobile Chrome',
+         use: { ...devices['Pixel 5'] },
+      }
    ]
 })
